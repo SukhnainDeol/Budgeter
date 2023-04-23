1a7897af5728c9df68806fdf37eaf839fc444267
Sukhnain Deol <dsukhnain@gmail.com>
2023-04-22 22:08:41 -0700

    creating rough draft of scene and min window size

--- src/main/java/com/application/budgeter/App.java
+++ src/main/java/com/application/budgeter/App.java
@@ -17,7 +17,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 900, 600);
+
+        // set current size to min
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
+
         stage.setScene(scene);
         stage.show();
     }
